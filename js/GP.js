20544eeb899cb26b389e3e4dbf92125c70771309
mohammedali230 <mhejiy@gmail.com>
2025-07-10 16:11:04 +0300

    add game page

---
+++ js/GP.js
@@ -0,0 +1,8 @@
+const backBtn=document.getElementById("backBtn")
+
+
+//make the button send to
+backBtn.addEventListener("click", ()=>
+{
+    window.location.href="index.html";
+})
---
+++ js/SP.js
@@ -0,0 +1,9 @@
+console.log("Working?")
+const sBtn=document.getElementById("StartBtn")
+
+
+//make the button send to
+sBtn.addEventListener("click", ()=>
+{
+    window.location.href="GP.html";
+})
