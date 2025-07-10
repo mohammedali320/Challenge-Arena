82966fe8d0c6f4d301aec38a438de1239f9f3c5d
mohammedali230 <mhejiy@gmail.com>
2025-07-10 16:44:59 +0300

    Score page added

--- js/GP.js
+++ js/GP.js
@@ -6,3 +6,12 @@ backBtn.addEventListener("click", ()=>
 {
     window.location.href="index.html";
 })
+
+const scoreBtn=document.getElementById("scoreBtn")
+
+
+//make the button send to
+scoreBtn.addEventListener("click", ()=>
+{
+    window.location.href="ScoreP.html";
+})
---
+++ js/ScoreP.js
@@ -0,0 +1,9 @@
+
+const reBtn=document.getElementById("resetBtn")
+
+
+//make the button send to
+reBtn.addEventListener("click", ()=>
+{
+    window.location.href="index.html";
+})
