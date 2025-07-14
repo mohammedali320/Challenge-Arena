6c9998d0111cda7564eef9e691de0f3e69735b49
mohammedali230 <mhejiy@gmail.com>
2025-07-14 11:01:58 +0300

    display each cat score in the score page

--- js/ScoreP.js
+++ js/ScoreP.js
@@ -10,3 +10,14 @@ reBtn.addEventListener("click", ()=>
 
 const totalScore = localStorage.getItem("totalScore") || 0;
 document.getElementById("totalScore").textContent = `Total Score: ${totalScore}`;
+
+const cat1 = localStorage.getItem("cat1Score") || 0;
+const cat2 = localStorage.getItem("cat2Score") || 0;
+const cat3 = localStorage.getItem("cat3Score") || 0;
+const cat4 = localStorage.getItem("cat4Score") || 0;
+
+document.querySelectorAll("p1")[0].textContent = `🏅 Sports score:          ${cat1}`;
+document.querySelectorAll("p1")[1].textContent = `🗺️ Capital Cities score:  ${cat2}`;
+document.querySelectorAll("p1")[2].textContent = `💻 Technology score:      ${cat3}`;
+document.querySelectorAll("p1")[3].textContent = `🧠 Logic & Riddles: score ${cat4}`; 
+
